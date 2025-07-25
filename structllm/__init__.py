55a1ae88c63300b99a44ca26b1bc12184a265ba1
Piotr Bednarski <piotr.maciej.bednarski@gmail.com>
2025-07-25 02:02:12 +0200

    chore: fix docstring formatting in __init__.py

--- structllm/__init__.py
+++ structllm/__init__.py
@@ -1,5 +1,7 @@
 """StructLLM - Universal Python library for Structured Outputs with any LLM provider."""
 
+"""StructLLM library initialization module."""
+
 from .client import StructLLM
 
 __version__ = "0.1.0"
